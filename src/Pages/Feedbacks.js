81b8bc1d4ab7783270375afadc0c647b91eeff08
matheusg18 <matheusgabriel.18@gmail.com>
2021-12-03 00:40:57 -0300

    Estiliza feedback page

--- src/Pages/Feedbacks.js
+++ src/Pages/Feedbacks.js
@@ -2,53 +2,53 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../Components/Header';
+import Button from '../Components/Button';
+import './Feedback.css';
 
 class Feedback extends Component {
   feedback() {
     const { assertions } = this.props;
     const tres = 3;
     if (assertions < tres) {
-      return 'Podia ser melhor...';
+      return 'Podia ser melhor... 😅';
     }
-    return 'Mandou bem!';
+    return 'Mandou bem!  🥳';
   }
 
   render() {
-    const { assertions, score, history } = this.props;
+    const { assertions, history } = this.props;
     return (
-      <div>
+      <>
         <Header />
-        <p data-testid="feedback-text">{ this.feedback() }</p>
-        <p data-testid="feedback-total-score">{ score }</p>
-        <p data-testid="feedback-total-question">{ assertions }</p>
-        <button
-          type="button"
-          data-testid="btn-play-again"
-          onClick={ () => history.push('/') }
-        >
-          Jogar novamente
-        </button>
-        <button
-          type="button"
-          data-testid="btn-ranking"
-          onClick={ () => history.push('/ranking') }
-        >
-          Ver Ranking
-        </button>
-      </div>
+        <div className="feedback-page">
+          <p data-testid="feedback-text">{ this.feedback() }</p>
+          <p data-testid="feedback-total-question">{ `Você acertou ${assertions}` }</p>
+          <div className="feedback-buttons">
+            <Button
+              testid="btn-play-again"
+              onClick={ () => history.push('/') }
+            >
+              Jogar novamente
+            </Button>
+            <Button
+              testid="btn-ranking"
+              onClick={ () => history.push('/ranking') }
+            >
+              Ver Ranking
+            </Button>
+          </div>
+        </div>
+      </>
     );
   }
 }
 
 const mapStateToProp = (state) => ({
   assertions: state.player.assertions,
-  score: state.player.score,
-
 });
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
-  score: PropTypes.number.isRequired,
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
